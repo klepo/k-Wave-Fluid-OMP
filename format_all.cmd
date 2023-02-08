023f1c541faa9f8174c03273d7c48db4628a3b40
Petr Klepárník <ikleparnik@fit.vutbr.cz>
2023-02-08 23:41:37 +0100

    #7: clang-format

---
+++ format_all.cmd
@@ -0,0 +1,14 @@
+@echo on
+clang-format -i ./Compression/*.cpp ./Compression/*.h
+clang-format -i ./Containers/*.cpp ./Containers/*.h
+clang-format -i ./Hdf5/*.cpp ./Hdf5/*.h
+clang-format -i ./KSpaceSolver/*.cpp ./KSpaceSolver/*.h
+clang-format -i ./Logger/*.cpp ./Logger/*.h
+clang-format -i ./MatrixClasses/*.cpp ./MatrixClasses/*.h
+clang-format -i ./OutputStreams/*.cpp ./OutputStreams/*.h
+clang-format -i ./Parameters/*.cpp ./Parameters/*.h
+clang-format -i ./Utils/*.h
+
+
+
+
